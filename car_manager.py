9f4457c735495b99395fcf75ee28e5503beca176
Ahmed Saleh <ahmed.ib3110@gmail.com>
2023-09-19 14:53:01 +0300

    Create car behavior

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,48 @@
+from turtle import Turtle
+from board import SC_UNIT, SC_WIDTH, SC_HEIGHT
+import random
+
 COLORS = []
 START_MOVE_DIST = 5
 MOVE_INCREMENT = 10
 
 
 class CarManager:
-    pass
+    def __init__(self):
+        # TODO => Create an empty list 'fleet', which will later store a number of 'cars'
+        self.fleet = []
+
+    def gen_fleet(self, num_of_cars):
+        for car in range(num_of_cars):
+            self.add_car()
+
+    def add_car(self):
+        # TODO => Create a car object using the 'Turtle' class
+        # Car shape should be a rectangle (use a square shape and shapesize() method to adjust the shape further)
+        new_car = Turtle("square")
+        new_car.shapesize(2, 1)
+        new_car.color(229, 229, 229)
+        new_car.setheading(90)
+        new_car.penup()
+        start_pos_rand = self.gen_random_pos()
+        new_car.goto(start_pos_rand)
+        # TODO => Add the car object to the 'fleet' list
+        self.fleet.append(new_car)
+
+    def move_fleet(self):
+        for car in self.fleet:
+            # TODO => Move the 'car' by a constant amount in the 'X' direction
+            car.goto(car.xcor() - START_MOVE_DIST, car.ycor())
+            # TODO => Move the 'car' to the other end of the screen, if it reaches the 'X' boundary
+            if car.xcor() < -SC_WIDTH:
+                car.goto(car.xcor() * -1, self.gen_random_pos()[1])
+
+    @staticmethod
+    def gen_random_pos():
+        x_rand = random.uniform(-SC_WIDTH, SC_WIDTH)
+        y_rand = random.uniform(-(SC_HEIGHT / 2), (SC_HEIGHT / 2))
+        random_pos = (x_rand, y_rand)
+        return random_pos
+
+
+
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
 from turtle import Screen
 from player import Player
+from car_manager import CarManager
 from board import SC_WIDTH, SC_HEIGHT
 
 screen = Screen()
@@ -19,7 +20,15 @@ screen.onkey(player.move_up, "Up")
 screen.onkey(player.jump_right, "Right")
 screen.onkey(player.jump_left, "Left")
 
+# TODO => Create car behavior
+# Car behavior is created through two methods, which are handled inside the 'CarManager' class:
+# 1) gen_fleet => generates a fleet of cars using a specified number of cars
+# 2) move_fleet => constantly moves each car inside the fleet
+car_man = CarManager()
+car_man.gen_fleet(24)
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
+    car_man.move_fleet()
     screen.update()
