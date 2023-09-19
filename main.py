d56a443528baadba835e1383250e138ae93df4ed
Ahmed Saleh <ahmed.ib3110@gmail.com>
2023-09-19 03:27:04 +0300

    Create a movable player object

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import time
 from turtle import Screen
+from player import Player
 from board import SC_WIDTH, SC_HEIGHT
 
 screen = Screen()
 screen.colormode(255)
 screen.setup(width=SC_WIDTH, height=SC_HEIGHT)
+screen.bgcolor(0, 0, 0)
 screen.title("Turtle Crossing")
 screen.tracer(0)
+
+# TODO => Create player object
+player = Player()
+
+# TODO => Create player movement
+screen.listen()
+screen.onkey(player.move_up, "Up")
+screen.onkey(player.jump_right, "Right")
+screen.onkey(player.jump_left, "Left")
 
 game_is_on = True
 while game_is_on:
--- player.py
+++ player.py
@@ -1,8 +1,26 @@
+from turtle import Turtle
 from board import SC_UNIT, SC_HEIGHT
 START_POS = (0, -((SC_HEIGHT / 2) - SC_UNIT))
 MOVE_DIST = 10
+JUMP_DIST = MOVE_DIST * 2
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.color(255, 255, 255)
+        self.setheading(90)
+        self.penup()
+        self.goto(START_POS)
+
+    def move_up(self):
+        self.goto(self.xcor(), self.ycor() + MOVE_DIST)
+
+    def jump_right(self):
+        self.goto(self.xcor() + JUMP_DIST, self.ycor())
+
+    def jump_left(self):
+        self.goto(self.xcor() - JUMP_DIST, self.ycor())
